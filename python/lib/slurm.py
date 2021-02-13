435826276913ff7e9d44dade1b911baf908001c3
Laurence <laurence+github@entek.org.uk>
2021-02-13 09:52:13 +0000

    Setup GitHub Actions for linting (#3)
    * Getting started with GitHub Actions
    
    * Errant tab
    
    * Python files need to end .py (well, be modules) for linter
    
    * Linting python
    
    * Linting python
    
    * Convert indent to 4 spaces for linter
    
    * Linting lib.slurm
    
    * Linting sh-estimate-cost.py
    
    * Linting sh-make-hostlist.py
    
    * Linting sh-about-me.py
    
    * More linting
    
    * More linting

---
+++ python/lib/slurm.py
@@ -0,0 +1,94 @@
+# Copyright 2020 Laurence Alexander Hurst
+#
+# This file is part of slurm-helpers.
+#
+# slurm-helpers is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# slurm-helpers is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with slurm-helpers.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+Provides some functions to interrogate and interact with Slurm.
+"""
+
+import subprocess
+
+# Amount to multiply budgets by to get pounds
+BUDGET_TO_POUNDS_FACTOR=1.0/1e8
+
+def capture_subprocess(command):
+    """
+    Run the specified command, capturing and returning the output.
+
+    Created as a function to have single point for this boiler-plate
+    code and stop pylint complaining about the arguments to
+    subprocess.run being duplicated code.
+    """
+    return subprocess.run(
+        command,
+        capture_output=True,
+        encoding='utf-8',
+        check=True,
+    )
+
+def find_account_budget_and_usage(account):
+    """
+    Finds the budget and total usage so far for an account.
+
+    args:
+        account: account to find details for
+
+    returns tuple of (budget, usage)
+    """
+    # Get the account, account's minutes budget and the usage
+    output = capture_subprocess(
+        [
+            "sshare",
+            "-A", account,
+            "-P",
+            "-n",
+            "-o", "account,GrpTRESMins,GrpTRESRaw",
+        ],
+    )
+    for line in output.stdout.split('\n'):
+        if not line:
+            continue # Skip blank lines
+        (account, tres_limit, tres_usage) = line.split('|')
+        tres_limit_dict = dict([i.split('=') for i in tres_limit.split(',')])
+        tres_usage_dict = dict([i.split('=') for i in tres_usage.split(',')])
+        return (int(tres_limit_dict['billing']), int(tres_usage_dict['billing']))
+
+def get_partition_info():
+    """
+    Find information about the slurm partitions from slurm.
+
+    args: None
+
+    returns a dict keyed on partition names to a dict of information
+    from slurm.
+    """
+    # Find the cost/node/minute for all partitions (it is one command,
+    # one or all does not matter too much - launching the sub-process
+    # is the expensive bit of this operation).
+    output = capture_subprocess(
+        [
+            'scontrol',
+            'show', 'partition',
+            '--oneline',
+        ],
+    )
+    part_dict = {}
+    for line in output.stdout.split('\n'):
+        if not line:
+            continue # Skip blank lines
+        part_info = dict([i.split('=', 1) for i in line.split(' ')])
+        part_dict[part_info['PartitionName']] = part_info
+    return part_dict
---
+++ python/sh-estimate-cost.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 Laurence Alexander Hurst
+#
+# This file is part of slurm-helpers.
+#
+# slurm-helpers is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# slurm-helpers is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with slurm-helpers.  If not, see <https://www.gnu.org/licenses/>.
+
+# Disable module-name and module doc-string checks - this is a script,
+# not a module
+#pylint: disable=C0103
+#pylint: disable=C0111
+
+import argparse
+import re
+import sys
+
+from lib.slurm import get_partition_info, BUDGET_TO_POUNDS_FACTOR
+
+
+def print_cost(nodes, duration, partitions=None):
+    """
+    Prints the estimated cost for a job.
+
+    args:
+        nodes: number of nodes being used
+        duration: estimated duration in minutes
+        partition: partition to print the cost for, prints all
+                     partitions if not specified or set to None
+    """
+    part_info = get_partition_info()
+    if partitions is None:
+        print_partitions = part_info.keys()
+    else:
+        print_partitions = partitions
+
+    for partition in print_partitions:
+        days = int(duration / 1440)
+        rem = duration - days*1440
+        hours = int(rem / 60)
+        minutes = rem - 60*hours
+        billing_weights = dict(
+            [
+                i.split('=') \
+                for i in part_info[partition]['TRESBillingWeights'].split(',')
+            ]
+        )
+        cost = int(billing_weights['Node']) * nodes * duration * \
+               BUDGET_TO_POUNDS_FACTOR
+        print(
+            "Running a %d node job in %s for %dd%02d:%02d will cost"
+            " approx £%0.2f" % (nodes, partition, days, hours, minutes, cost)
+        )
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Display estimated cost for your job."
+    )
+    parser.add_argument(
+        "--partition", "-p",
+        action="append",
+        help="Partition you wish to use (may be specified multiple times for" \
+             " more than 1 partition, default: show price for all" \
+             " partitions).",
+    )
+    parser.add_argument(
+        "--nodes", "-n",
+        action="store",
+        type=int,
+        default=1,
+        help="Number of nodes you wish to use (default: 1).",
+    )
+    parser.add_argument(
+        "--duration", "-d",
+        action="store",
+        default='1',
+        help="Estimated duration of your job (format: [Dd]H[:M], e.g. 5d2:26" \
+             " for 5 days, 2 hours and 26 minutes, default: 1 (= 1 hour)).",
+    )
+
+    args = parser.parse_args()
+
+    time_regx = re.compile(
+        r'^(:?(?P<days>[0-9]+)d)?(?P<hours>[0-9]+)(:?:(?P<minutes>[0-9]+))?$'
+    )
+    match = time_regx.match(args.duration)
+    if not match:
+        print(
+            "Error parsing duration, please check it is in the right format.",
+            file=sys.stderr,
+        )
+        parser.print_help(file=sys.stderr)
+        sys.exit(1)
+    else:
+        # Hours are mandatory, so start with that value
+        job_duration = 60 * int(match['hours'])
+        # ...plus days
+        if match['days'] is not None:
+            # 1,440 = 24 hours * 60 minutes
+            job_duration += 1440 * int(match['days'])
+        # ...plus minutes
+        if match['minutes'] is not None:
+            job_duration += int(match['minutes'])
+
+    print_cost(args.nodes, job_duration, args.partition)
